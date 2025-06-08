149fed7c70ae3af23535d6349cd81066a88f3c12
nikolai <sportislife635@gmail.com>
2025-06-08 23:26:01 +0300

    Видео 26 Проброс луча для определения положения появления монстра

--- scene/manager/spawner/enemy_spawner.gd
+++ scene/manager/spawner/enemy_spawner.gd
@@ -13,19 +13,32 @@ func _ready() -> void:
 	base_spawn_time = timer.wait_time
 	arena_time_manager.difficulty_increased.connect(on_difficulty_increased)
 
+func get_spawn_position():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	var spawn_pos = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	var random_distance = randi_range(300, 500)
+	for i in 4:
+		spawn_pos = player.global_position + (random_direction * random_distance)
+		#Пересечение луча 2д
+		var raycast = PhysicsRayQueryParameters2D.create(player.global_position, spawn_pos, 1)
+		var intersection = get_tree().root.world_2d.direct_space_state.intersect_ray(raycast)
+		if intersection.is_empty():
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+	return spawn_pos
+
 func _on_timer_timeout() -> void:
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var random_distance = randi_range(300, 500)
-	var spawn_pos = player.global_position + (random_direction * random_distance)
 	
 	var enemy = plus_monster_scene.instantiate() as Node2D
 	var back_layer = get_tree().get_first_node_in_group("back_layer")
 	back_layer.add_child(enemy)
 	
-	enemy.global_position = spawn_pos
+	enemy.global_position = get_spawn_position()
 
 func on_difficulty_increased(difficulty_level:int):
 	var new_spawn_time = max(base_spawn_time - (difficulty_level * difficulty_multiplier), min_spawn_time)
