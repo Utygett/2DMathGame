990fce8d759cffee20e5d06ea96e8996b8b92da5
nikolai <sportislife635@gmail.com>
2025-06-08 20:25:44 +0300

    Видео 23 Меню победы и поражения, Добавление кнопок в UI, Изменение текста в лейбле.

---
+++ scene/levels/level.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var end_screen_scene: PackedScene
+
+@onready var player: CharacterBody2D = %Player
+
+func _ready() -> void:
+	player.health_component.died.connect(on_died)
+	
+func on_died():
+	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
+	add_child(end_screen_instance)
--- scene/manager/arena_time_manager.gd
+++ scene/manager/arena_time_manager.gd
@@ -1,6 +1,14 @@
 extends Node
 
+@export var end_screen_scene: PackedScene
+
 @onready var timer = $Timer
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func _on_timer_timeout() -> void:
+	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
+	get_parent().add_child(end_screen_instance)
+	end_screen_instance.change_to_victory()
