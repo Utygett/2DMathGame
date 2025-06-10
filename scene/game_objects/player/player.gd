b5e05322c2fbddda8d4debaf3b62dcf69ade1c6d
nikolay <sportislife635@gmail.com>
2025-06-11 00:05:32 +0300

    Видео 28 Прокачка нового скила, добавления новой сцены к игрку, через скрипт

--- scene/game_objects/player/player.gd
+++ scene/game_objects/player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var grace_period: Timer = $GracePeriod
 @onready var progress_bar: ProgressBar = $ProgressBar
+@onready var ability_manager: Node = $AbilityManager
+
 
 var s_max_speed = 100
 var s_acceleration = 0.15
@@ -10,6 +12,7 @@ var enemies_colliding = 0
 func _ready() -> void:
 	health_component.died.connect(on_died)
 	health_component.health_change.connect(on_health_changed)
+	Global.ability_upgrage_added.connect(on_ability_upgrade_added)
 	health_update()
 
 func _process(_delta: float) -> void:
@@ -50,3 +53,10 @@ func on_health_changed():
 
 func _on_grace_period_timeout() -> void:
 	check_if_damage()
+
+func on_ability_upgrade_added(upgrade:AbilityUpgrade, current_upgrades: Dictionary):
+	if not upgrade is NewAbility:
+		return
+	
+	var new_ability = upgrade as NewAbility
+	ability_manager.add_child(new_ability.new_ability_scene.instantiate())
--- scene/manager/upgrade_manager.gd
+++ scene/manager/upgrade_manager.gd
@@ -9,16 +9,6 @@ var cur_upgrdaes = {}
 func _ready() -> void:
 	exp_manager.level_up.connect(on_level_up)
 	
-func on_level_up(cur_level):
-	var choosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
-	if choosen_upgrade == null:
-		return
-		
-	var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
-	add_child(upgrade_screen_instance)
-	upgrade_screen_instance.set_ability_upgrades([choosen_upgrade] as Array[AbilityUpgrade])
-	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
-
 	
 func apply_upgrade(upgrade: AbilityUpgrade):
 	var has_upgrade = cur_upgrdaes.has(upgrade.id)
@@ -32,5 +22,24 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	Global.ability_upgrage_added.emit(upgrade, cur_upgrdaes)
 		
 	
+
+func pick_upgrades():
+	var chosen_upgrades: Array[AbilityUpgrade]
+	var pool_copy = upgrade_pool.duplicate()
+	for i in 2:
+		var chosen_upgrade = pool_copy.pick_random() as AbilityUpgrade
+		chosen_upgrades.append(chosen_upgrade)
+		pool_copy = pool_copy.filter(func(upgrade): return upgrade.id != chosen_upgrade.id)
+		
+	return chosen_upgrades
+
+
 func on_upgrade_selected(upgrade:AbilityUpgrade):
 	apply_upgrade(upgrade)
+
+func on_level_up(cur_level):
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate() as UpgradeScreen
+	add_child(upgrade_screen_instance)
+	var chosen_upgrades = pick_upgrades()
+	upgrade_screen_instance.set_ability_upgrades(chosen_upgrades as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
