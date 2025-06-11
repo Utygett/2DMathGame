bdca1b41fcbeb0a4f70e5e17fb26467e688e577f
nikolai <sportislife635@gmail.com>
2025-06-11 18:08:47 +0300

    Видео 32 Анимация смерти подключена к монстрам, замена спрайта к общему ноду.

--- scene/game_objects/enemy/plus_monster.gd
+++ scene/game_objects/enemy/plus_monster.gd
@@ -4,6 +4,9 @@ var s_max_speed = 40
 
 @onready var health_component: Node = $HealthComponent
 
+@export var death_scene: PackedScene
+@export var sprite: CompressedTexture2D
+
 func _ready() -> void:
 	health_component.died.connect(on_died)
 
@@ -20,4 +23,9 @@ func get_direction_to_player():
 	return Vector2.ZERO
 	
 func on_died():
+	var back_layer = get_tree().get_first_node_in_group("back_layer")
+	var death_instance = death_scene.instantiate() as DeathComp
+	back_layer.add_child(death_instance)
+	death_instance.gpu_particles_2d.texture = sprite
+	death_instance.global_position = global_position
 	queue_free()
