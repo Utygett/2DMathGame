f66034bf1c7fe48e5c0199fa232ac814aca7d336
nikolai <sportislife635@gmail.com>
2025-06-12 20:04:03 +0300

    Видео 43 Пропуск сигнала через элементы ui, анимация прозрачного фона при появлении умений.

--- scene/ui/upgrade_screen.gd
+++ scene/ui/upgrade_screen.gd
@@ -6,6 +6,7 @@ signal upgrade_selected(upgrade:AbilityUpgrade)
 @export var upgrade_card_scene: PackedScene
 
 @onready var card_container: HBoxContainer = %CardContainer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	get_tree().paused = true
@@ -23,5 +24,7 @@ func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
 
 func on_upgrade_selected(upgrade:AbilityUpgrade): 
 	upgrade_selected.emit(upgrade)
+	animation_player.play("out")
+	await animation_player.animation_finished
 	get_tree().paused = false
 	queue_free()
