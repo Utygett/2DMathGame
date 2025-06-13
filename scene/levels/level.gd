66f4ca8de43b83b1614ccfda157218cdf1cf4aef
nikolai <sportislife635@gmail.com>
2025-06-13 22:19:46 +0300

    Видео 53 Pause меню

--- scene/levels/level.gd
+++ scene/levels/level.gd
@@ -4,6 +4,8 @@ extends Node
 
 @onready var player: CharacterBody2D = %Player
 
+var pause_menu_scene = preload("res://scene/ui/pause_menu.tscn")
+
 func _ready() -> void:
 	Bgm.play()
 	player.health_component.died.connect(on_died)
@@ -12,3 +14,7 @@ func on_died():
 	var end_screen_instance = end_screen_scene.instantiate() as EndScreen
 	add_child(end_screen_instance)
 	end_screen_instance.play_jingle()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
